32b67490eeb09956c933d98603bcf6e5489f1868
Ryan Hanson <hansonry@gmail.com>
2016-06-17 16:26:02 -0500

    Start work on a main menu

--- src/main.lua
+++ src/main.lua
@@ -1,24 +1,33 @@
 local game = require('game')
-
+local menu = require('menu')
 
 function love.load()
+   state = "game"
    game.load()
 end
 
 
 function love.keypressed(key)
-   game.keypressed(key)
+   if state == "game" then
+      game.keypressed(key)
+   end
 end
 
 function love.keyreleased(key)
-   game.keyreleased(key)
+   if state == "game" then
+      game.keyreleased(key)
+   end
 end
 
 function love.update(dt)
-   game.update(dt)
+   if state == "game" then
+      game.update(dt)
+   end
 end
 
 function love.draw()
-   game.draw()
+   if state == "game" then
+      game.draw()
+   end
 end
 
---
+++ src/menu.lua
@@ -0,0 +1,26 @@
+
+
+local function load()
+end
+
+
+local function keypressed(key)
+end
+
+local function keyreleased(key)
+end
+
+local function update(dt)
+end
+
+local function draw()
+end
+
+return {
+   load        = load,
+   keypressed  = keypressed,
+   keyreleased = keyreleased,
+   update      = update,
+   draw        = draw,
+}
+
