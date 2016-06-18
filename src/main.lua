70d8292f149011e1abd760a5d6f2350a98d45a9d
Ryan Hanson <hansonry@gmail.com>
2016-06-18 10:48:47 -0500

    Added Title Image

--- src/main.lua
+++ src/main.lua
@@ -2,32 +2,41 @@ local game = require('game')
 local menu = require('menu')
 
 function love.load()
-   state = "game"
+   state = "menu"
    game.load()
+   menu.load()
 end
 
 
 function love.keypressed(key)
    if state == "game" then
       game.keypressed(key)
+   elseif state == "menu" then
+      menu.keypressed(key)
    end
 end
 
 function love.keyreleased(key)
    if state == "game" then
       game.keyreleased(key)
+   elseif state == "menu" then
+      menu.keyreleased(key)
    end
 end
 
 function love.update(dt)
    if state == "game" then
       game.update(dt)
+   elseif state == "menu" then
+      state = menu.update(dt)
    end
 end
 
 function love.draw()
    if state == "game" then
       game.draw()
+   elseif state == "menu" then
+      menu.draw()
    end
 end
 
--- src/menu.lua
+++ src/menu.lua
@@ -1,19 +1,31 @@
 
 
 local function load()
+   title = love.graphics.newImage("assets/title.png")
+   startgame = false
 end
 
 
 local function keypressed(key)
+   
 end
 
 local function keyreleased(key)
+   if key == " " then
+      startgame = true
+   end
 end
 
 local function update(dt)
+   if startgame then
+      return "game"
+   else
+      return "menu"
+   end
 end
 
 local function draw()
+   love.graphics.draw(title, 10, 10);
 end
 
 return {
